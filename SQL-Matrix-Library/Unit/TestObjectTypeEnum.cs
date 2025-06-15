39c989bb06d1fd1bbdbe6ad83363c05a43d321c3
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-06-15 21:15:09 +0200

    Create TestObjectTypeEnum.cs

---
+++ SQL-Matrix-Library/Unit/TestObjectTypeEnum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Matrix.MsSql.Unit
+{
+    /// <summary>
+    /// Represents the SQL object type to be tested.
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter<TestObjectTypeEnum>))]
+    public enum TestObjectTypeEnum
+    {
+        /// <summary>
+        /// Not identified.
+        /// </summary>
+        /// <remarks>Should be used only as fallback value, not for new instances.</remarks>
+        None = 0,
+
+        /// <summary>
+        /// SQL scalar function.
+        /// </summary>
+        ScalarFunction = 1,
+
+        /// <summary>
+        /// SQL table valued function.
+        /// </summary>
+        TableValuedFunction = 2,
+
+        /// <summary>
+        /// SQL stored procedure.
+        /// </summary>
+        StoredProcedure = 3
+    }
+}
