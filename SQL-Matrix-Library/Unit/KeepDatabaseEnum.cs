8e7a1ed4a0986a37e92dc149be6f53ad83285dbf
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-06-15 23:39:00 +0200

    Create KeepDatabaseEnum.cs

---
+++ SQL-Matrix-Library/Unit/KeepDatabaseEnum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Matrix.MsSql.Unit
+{
+    /// <summary>
+    /// Represents the instruction, if the database sould be kept after the complete test run.
+    /// </summary>
+    public enum KeepDatabaseEnum
+    {
+        /// <summary>
+        /// Drops the test databse after the test run in any case.
+        /// </summary>
+        DropAlways = 0,
+
+        /// <summary>
+        /// Keeps the test databse only in case if one test fails. If all test runs are successful the test database will be dropped.
+        /// </summary>
+        /// <remarks>
+        /// In case of a failed test, the test scenario can be repeated on the database manually.
+        /// Therefore the test data of the failed run will be kept in the data tables.
+        /// </remarks>
+        KeepOnFailure = 1,
+
+        /// <summary>
+        /// Keeps the test database after the test run in any case.
+        /// </summary>
+        /// <remarks>
+        /// Test data from the data tables will be removed.
+        /// </remarks>
+        KeepAlways = 2
+    }
+}
