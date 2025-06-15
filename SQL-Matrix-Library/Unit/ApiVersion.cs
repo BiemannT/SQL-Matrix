3f9051c38184beb0229a3ec86baa0498b2d727a6
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-06-15 21:14:29 +0200

    Create ApiVersion.cs

---
+++ SQL-Matrix-Library/Unit/ApiVersion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Matrix.MsSql.Unit
+{
+    /// <summary>
+    /// Represents the necessary API version of this test file.
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter<ApiVersion>))]
+    public enum ApiVersion
+    {
+        /// <summary>
+        /// Not identified.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Version v1.0.0
+        /// </summary>
+        v1 = 1
+    }
+}
