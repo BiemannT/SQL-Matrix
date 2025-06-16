2869981b400e1647a73768207eefd10b22a995cb
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-06-17 00:43:44 +0200

    Setup and testing SQL connection

--- SQL-Matrix-Unit-CLI/Main.cs
+++ SQL-Matrix-Unit-CLI/Main.cs
@@ -1,4 +1,6 @@
-﻿using System.Security;
+﻿using Microsoft.Data.SqlClient;
+using System.Net;
+using System.Security;
 using System.Text.Json;
 
 namespace Matrix.MsSql.Unit
@@ -26,13 +28,22 @@ namespace Matrix.MsSql.Unit
         ///             <description>2</description>
         ///             <description>Error during reading the json test file.</description>
         ///         </item>
+        ///         <item>
+        ///             <description>3</description>
+        ///             <description>Error during SQL connections.</description>
+        ///         </item>
         ///     </list>
         /// </remarks>
         static void Main(string[] args)
         {
             FileInfo TestFile;
-            TestDefinition? definition;
+            TestDefinition? definition = new();
             IPEndPoint SqlEndpoint;
+            string SqlInstance;
+            string SqlLoginName;
+            SecureString SqlLoginPassword;
+            SqlCredential SqlCredential;
+            SqlConnectionStringBuilder SqlConnBuilder;
 
             // Trigger für den Abbruch
             Console.CancelKeyPress += Console_CancelKeyPress;
@@ -136,6 +147,129 @@ namespace Matrix.MsSql.Unit
                 Environment.Exit(2);
             }
 
+            // Verbindung zum SQL-Server herstellen
+            Console.WriteLine("Setup SQL-Connection");
+
+            // Server Hostname
+            string SqlHost;
+            IPHostEntry SqlHostEntry = new();
+
+            do
+            {
+                Console.WriteLine("Enter Server Hostname or IPv4 Address:");
+                SqlHost = Console.ReadLine() ?? string.Empty;
+                    
+                if (!string.IsNullOrWhiteSpace(SqlHost))
+                {
+                    try
+                    {
+                        SqlHostEntry = Dns.GetHostEntry(SqlHost, System.Net.Sockets.AddressFamily.InterNetwork);
+                    }
+                    catch
+                    {
+                        Console.WriteLine($"Could not find {SqlHost}.");
+                        SqlHost = string.Empty;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No input. Try again.");
+                    SqlHost = string.Empty;
+                }
+            }
+            while (string.IsNullOrWhiteSpace(SqlHost));
+
+            // Server Port
+            do
+            {
+                Console.WriteLine("Enter the port number on which the SQL-Server is listening (default: 1433):");
+                if (int.TryParse(Console.ReadLine(), out int SqlPort))
+                {
+                    try
+                    {
+                        SqlEndpoint = new IPEndPoint(SqlHostEntry.AddressList[0], SqlPort);
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        Console.WriteLine($"The port number {SqlPort} is not supported. Try again.");
+                        continue;
+                    }
+
+                    // SQL-Server Adresse erfolgreich gesetzt
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("The entered value is not a valid number. Try again.");
+                    continue;
+                }
+
+            } while (true);
+
+            // Optional: Server Instanz Name
+            Console.WriteLine("Enter optionally the SQL-Server instance name. Left blank if no named instances installed.");
+            SqlInstance = Console.ReadLine() ?? string.Empty;
+
+            // SQL Anmeldename
+            Console.WriteLine("Enter the login name (Default name 'sa', if left blank):");
+            SqlLoginName = Console.ReadLine() ?? "sa";
+
+            // SQL Login Passwort
+            ConsoleKeyInfo key;
+            SqlLoginPassword = new();
+            Console.WriteLine($"Enter the password for the login name {SqlLoginName}:");
+            do
+            {
+                key = Console.ReadKey(true);
+                // Nur druckbare Zeichen zulassen
+                if (((int) key.Key) >= 32)
+                {
+                    SqlLoginPassword.AppendChar(key.KeyChar);
+                    Console.Write("*");
+                }
+
+            } while (key.Key != ConsoleKey.Enter);
+            SqlLoginPassword.MakeReadOnly();
+            Console.Write(Environment.NewLine);
+
+            // SQL-Connection aufbereiten
+            SqlCredential = new(SqlLoginName, SqlLoginPassword);
+            SqlConnBuilder = new()
+            {
+                Authentication = SqlAuthenticationMethod.SqlPassword,
+                ConnectRetryCount = 3,
+                ConnectRetryInterval = 5,
+                ConnectTimeout = 10,
+                DataSource = $"tcp:{SqlEndpoint.Address},{SqlEndpoint.Port}{(string.IsNullOrWhiteSpace(SqlInstance) ? "" : string.Concat("\\", SqlInstance))}",
+                IPAddressPreference = SqlConnectionIPAddressPreference.IPv4First,
+                IntegratedSecurity = false,
+                PersistSecurityInfo = false,
+                TrustServerCertificate = true
+            };
+
+            definition.SqlConn = new(SqlConnBuilder.ConnectionString, SqlCredential);
+            using (definition.SqlConn)
+            {
+                string TestConnCmd = "SELECT GETDATE();";
+                SqlCommand cmd = new(TestConnCmd, definition.SqlConn)
+                {
+                    CommandTimeout = 10
+                };
+
+                cmd.Connection.Open();
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch
+                {
+                    Console.WriteLine("WARNING: Failed to connect to the SQL-Server!");
+                    Environment.Exit(3);
+                }
+            }
+            Console.WriteLine($"Connection to the SQL-Server {SqlHostEntry.HostName} established successfully.");
+
+            SqlLoginPassword.Dispose();
             Console.ReadLine();
         }
 
