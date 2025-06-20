9e80bc0a4d39297483e3e83222fc953d81cd601e
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-06-20 16:11:19 +0200

    Create DacPublishMessageTypeEnum.cs

---
+++ SQL-Matrix-Library/DacPublishMessageTypeEnum.cs
@@ -0,0 +1,23 @@
+﻿namespace Matrix.MsSql
+{
+    /// <summary>
+    /// Specifies the type (or severity) of message associated with a given event.
+    /// </summary>
+    public enum DacPublishMessageTypeEnum
+    {
+        /// <summary>
+        /// Informational message.
+        /// </summary>
+        Message = 0,
+
+        /// <summary>
+        /// Noncritical problem.
+        /// </summary>
+        Warning = 1,
+
+        /// <summary>
+        /// Serious, possibly fatal issue.
+        /// </summary>
+        Error = 2
+    }
+}
