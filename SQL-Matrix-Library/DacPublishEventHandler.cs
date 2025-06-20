8eef8e2c1c563f97431f52b20209e01af230b949
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-06-20 17:16:43 +0200

    Create DacPublishEventHandler.cs

---
+++ SQL-Matrix-Library/DacPublishEventHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace Matrix.MsSql
+{
+    /// <summary>
+    /// Represents the method that handles the <see cref="SetupDacService.ProgressMessage"/> event.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">An <see cref="DacPublishEventArgs"/> instance with detailed information containing the event data.</param>
+    public delegate void DacPublishEventHandler(object sender, DacPublishEventArgs e);
+}
