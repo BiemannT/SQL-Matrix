13b03f8ce9761073e216683d74e295f21ba0a547
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-06-29 21:23:50 +0200

    Add method BuiltinTinyInt

--- SQL-Matrix-Library/Unit/BuiltinTestValues.cs
+++ SQL-Matrix-Library/Unit/BuiltinTestValues.cs
@@ -264,5 +264,24 @@ namespace Matrix.MsSql.Unit
 
             return retValues;
         }
+
+        /// <summary>
+        /// Represents built-in test values for the type <see cref="SqlDbType.TinyInt"/>.
+        /// </summary>
+        /// <param name="size">Not required for this type.</param>
+        /// <param name="precision">Not required for this type.</param>
+        /// <param name="scale">Not required for this type.</param>
+        /// <returns>Returns an array with test values.</returns>
+        public static Array BuiltinTinyInt (int size = 0, byte precision = 0, byte scale = 0)
+        {
+            _ = size;
+            _ = precision;
+            _ = scale;
+            Byte[] retValues;
+
+            retValues = [0, 255];
+
+            return retValues;
+        }
     }
 }
