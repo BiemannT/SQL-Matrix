7b1f19dedbdab8974453ab71328ec7b212162425
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-06-29 22:39:30 +0200

    Add method BuiltinReal

--- SQL-Matrix-Library/Unit/BuiltinTestValues.cs
+++ SQL-Matrix-Library/Unit/BuiltinTestValues.cs
@@ -428,5 +428,25 @@ namespace Matrix.MsSql.Unit
 
             return retValues;
         }
+
+        /// <summary>
+        /// Represents built-in test values for the type <see cref="SqlDbType.Real"/>.
+        /// </summary>
+        /// <param name="size">Not required for this type.</param>
+        /// <param name="precision">Not required for this type.</param>
+        /// <param name="scale">Not required for this type.</param>
+        /// <returns>Returns an array with test values.</returns>
+        public static Array BuiltinReal (int size = 0, byte precision = 0, byte scale = 0)
+        {
+            _ = size;
+            _ = precision;
+            _ = scale;
+            Single[] retValues;
+
+            retValues = [0];
+            // TODO: Werte definieren
+
+            return retValues;
+        }
     }
 }
