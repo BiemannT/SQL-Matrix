f62fdffabe93e4335b091c496080551d19995f77
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-06-29 20:15:15 +0200

    Add method BuiltinVarbinary

--- SQL-Matrix-Library/Unit/BuiltinTestValues.cs
+++ SQL-Matrix-Library/Unit/BuiltinTestValues.cs
@@ -44,6 +44,45 @@ namespace Matrix.MsSql.Unit
 
             return retValues;
         }
+
+        /// <summary>
+        /// Represents built-in test values for the type <see cref="SqlDbType.VarBinary"/>.
+        /// </summary>
+        /// <param name="size">The size of the type from 1 through 8000. Set -1 to test VARBINARY(MAX). Set 0 for the default value 1.</param>
+        /// <param name="precision">Not required for this type.</param>
+        /// <param name="scale">Not required for this type.</param>
+        /// <returns>Returns an array with test values.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Will be thrown if <paramref name="size"/> is not between 1 and 8000 or -1 or 0.</exception>
+        public static Array BuiltinVarbinary (int size, byte precision = 0, byte scale = 0)
+        {
+            _ = precision;
+            _ = scale;
+            byte[] retValues;
+
+            if (size == 0)
+            {
+                // Default Value = 1
+                retValues= [];
+                // TODO: Werte definieren
+            }
+            else if (size == -1)
+            {
+                // VARBINARY(MAX)
+                retValues = [];
+                // TODO: Werte definieren
+            }
+            else if (size < 1 || size > 8000)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size), "The size must be a value from 1 through 8000.");
+            }
+            else
+            {
+                retValues = [];
+                // TODO: Werte definieren
+            }
+
+            return retValues;
+        }
         /// <summary>
         /// Represents built-in test values for the type <see cref="SqlDbType.VarChar"/>.
         /// </summary>
