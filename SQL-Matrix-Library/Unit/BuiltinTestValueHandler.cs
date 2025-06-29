b2f7e9348953f3bab2b27be90dfcf0a5cff296cd
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-06-29 17:49:13 +0200

    Create BuiltinTestValueHandler.cs

---
+++ SQL-Matrix-Library/Unit/BuiltinTestValueHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Matrix.MsSql.Unit
+{
+    /// <summary>
+    /// Encapsulates a method to retrieve built-in test values.
+    /// </summary>
+    /// <param name="size">The size required for the SQL-type.</param>
+    /// <param name="precision">The precision required for the SQL-type.</param>
+    /// <param name="scale">The scale required for the SQL-type.</param>
+    /// <returns>Returns an array with test values.</returns>
+    public delegate Array BuiltinTestValueHandler(int size, byte precision, byte scale);
+}
