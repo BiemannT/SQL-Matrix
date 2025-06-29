2fa873feef45a4208cca09992f1f49ebe48c9828
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-06-29 20:34:55 +0200

    Add method BuiltinChar

--- SQL-Matrix-Library/Unit/BuiltinTestValues.cs
+++ SQL-Matrix-Library/Unit/BuiltinTestValues.cs
@@ -83,6 +83,39 @@ namespace Matrix.MsSql.Unit
 
             return retValues;
         }
+
+        /// <summary>
+        /// Represents built-in test values for the type <see cref="SqlDbType.Char"/>.
+        /// </summary>
+        /// <param name="size">The size of the type from 1 through 8000. Set 0 for the default value 1.</param>
+        /// <param name="precision">Not required for this type.</param>
+        /// <param name="scale">Not required for this type.</param>
+        /// <returns>Returns an array with test values.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Will be thrown if <paramref name="size"/> is not between 1 and 8000 or 0.</exception>
+        public static Array BuiltinChar (int size, byte precision = 0, byte scale = 0)
+        {
+            _ = precision;
+            _ = scale;
+            string[] retValues;
+
+            if (size == 0)
+            {
+                // Default Value = 1
+                retValues = [" ", "a"];
+            }
+            else if (size < 1 || size > 8000)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size), "The size must be a value from 1 through 8000.");
+            }
+            else
+            {
+                retValues = [];
+                // TODO: Werte definieren
+            }
+
+            return retValues;
+        }
+
         /// <summary>
         /// Represents built-in test values for the type <see cref="SqlDbType.VarChar"/>.
         /// </summary>
