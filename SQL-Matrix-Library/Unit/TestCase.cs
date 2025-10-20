2c09b175eae36e4a0f6f6175826ad4001f74ddef
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-10-20 23:08:57 +0200

    Create TestCase.cs

---
+++ SQL-Matrix-Library/Unit/TestCase.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Matrix.MsSql.Unit
+{
+    /// <summary>
+    /// This class provides properties and methods to represent one testcase.
+    /// </summary>
+    public class TestCase
+    {
+        /// <summary>
+        /// Initialize a new instance of the <see cref="TestCase"/>-class with default values and without parameters.
+        /// </summary>
+        public TestCase()
+        {
+            this._State = TestCaseStateEnum.Initialized;
+            this.Parameters = [];
+        }
+
+        /// <summary>
+        /// Initialize a new instance of the <see cref="TestCase"/>-class with default values and the given parameter set.
+        /// </summary>
+        /// <param name="parameters">An array of <see cref="TestCaseParameter"/>-instances which represents the used input parameters for this test case.</param>
+        public TestCase(TestCaseParameter[] parameters)
+        {
+            this._State = TestCaseStateEnum.Initialized;
+            this.Parameters = parameters;
+        }
+
+        // TODO: Testcase definieren
+        // Ein Testcase besteht aus 0 oder mehreren Parametern.
+        // Diese Paremeter können vordefinierte Werte enthalten, oder benutzerdefinierte Werte.
+        // Ein Testcase kann 0 oder mehr Tabellen zur Ausführung erfordern.
+        // Dabei ist auch möglich, dass für ein und die selbe Tabelle unterschiedliche Datensätze betrachtet werden sollen.
+        // Für diesen einen Testcase gibt es erwartete Ergebnisse.
+        // Das Ergebnis kann entweder nur ein Rückgabewert einer Prozedur, ein Skalarwert einer Funktion, eine bestimmte Ausnahme oder ein oder mehrere Tabellen als Resultset sein.
+
+        // TODO: Testcase Aktionen
+        // Methode bereitstellen, um auf einer vorgegebenen SqlConnection den Test auszuführen.
+        // Eigenschaft erstellen, über den Zustand der Ausführung mit der Enumeration: Vorbereitet, Erwartetes Ergebnis fehlt, Eingabe Parameter fehlerhaft, Fehler bei der Erstellung der Bezugsdaten, Ausführung läuft, Erfolgreich-Ausgeführt, Fehlerhaft-Ausgeführt, Command-Timeout.
+        // Ereignis erstellen mit Informationen über die aktuelle Ausführung.
+        private TestCaseStateEnum _State;
+
+        /// <summary>
+        /// Gets the current state of this test case.
+        /// </summary>
+        public TestCaseStateEnum State
+        {
+            get => _State;
+        }
+
+        /// <summary>
+        /// Gets the collection of parameters associated with this test case.
+        /// </summary>
+        public TestCaseParameter[] Parameters { get; private set; }
+
+        public void ExecuteTest(SqlConnection connection)
+        {
+            // TODO: ExecuteTest Prozedur ausarbeiten
+            throw new NotImplementedException();
+        }
+
+        // TODO: Dokumentationsfunktionen implementieren
+        // Wünschenswert wäre eine Ausgabe im md- oder html-Format.
+    }
+}
