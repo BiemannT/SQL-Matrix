3018b6c27d9ed91185617cbf3727c345b6257bc0
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-11-02 19:27:16 +0100

    Create JsonDefinition.cs

---
+++ JSON/JSON/JsonDefinition.cs
@@ -0,0 +1,118 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BiemannT.MUT.MsSql.Def.JSON
+{
+    /// <summary>
+    /// This class provides properties and methods to interact with JSON-definition files.
+    /// </summary>
+    public class JsonDefinition : Base.Definition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JsonDefinition"/> class with default values.
+        /// </summary>
+        public JsonDefinition() {
+            // Initialize the class with default values.
+            _apiVersionJson = "v1";
+            SchemaName = string.Empty;
+            ObjectName = string.Empty;
+            _maxExecutionTime = 10;
+        }
+
+        [JsonPropertyName("SQL-Matrix-Api-Version")]
+        [JsonPropertyOrder(-10)]
+        [JsonRequired]
+        [JsonInclude]
+        private string _apiVersionJson;
+
+        /// <summary>
+        /// Gets or sets the API version for the current instance.
+        /// </summary>
+        /// <value>The API version. The default value is 1.0</value>
+        /// <remarks>This property is required!</remarks>
+        /// <exception cref="NotSupportedException">Currently version 1.0 is supported.</exception>
+        [JsonIgnore]
+        public override Version ApiVersion
+        {
+            get => _apiVersionJson switch
+            {
+                "v1" => new Version(1, 0),
+                _ => throw new NotSupportedException($"The API-Version '{_apiVersionJson}' is not supported."),
+            };
+
+            protected set
+            {
+                _apiVersionJson = value.Major switch
+                {
+                    1 => "v1",
+                    _ => throw new NotSupportedException($"The API-Version '{value}' is not supported."),
+                };
+            }
+            
+        }
+
+        /// <summary>
+        /// Gets or sets the schema name of the tested object.
+        /// </summary>
+        /// <value>The schema name of the tested object. The default value is <see cref="string.Empty"/>.</value>
+        /// <remarks>This property is required!</remarks>
+        [JsonPropertyName("SchemaName")]
+        [JsonPropertyOrder(-9)]
+        [JsonRequired]
+        public override string SchemaName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the tested object.
+        /// </summary>
+        /// <value>The name of the tested object. The default value is <see cref="string.Empty"/>.</value>
+        /// <remarks>This property is required!</remarks>
+        [JsonPropertyName("TestObjectName")]
+        [JsonPropertyOrder(-8)]
+        [JsonRequired]
+        public override string ObjectName { get; set; }
+
+        [JsonPropertyName("MaxExecutionTime")]
+        [JsonPropertyOrder(-7)]
+        [JsonRequired]
+        [JsonInclude]
+        private int _maxExecutionTime;
+
+        /// <summary>
+        /// Gets or sets the maximum execution time for each test case in seconds.
+        /// </summary>
+        /// <value>
+        /// The maximum execution time in seconds.
+        /// The default value is 10 seconds. The value 0 means infinite execution time.
+        /// It is not recommended to set the value to 0 for test cases as the test case can freeze the overall testing.
+        /// </value>
+        /// <remarks>This property is required and must be set to a positive integer value.</remarks>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the value is less than zero.</exception>"
+        [JsonIgnore]
+        public override int MaxExecutionTime
+        {
+            get => _maxExecutionTime;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "MaxExecutionTime must be a positive integer.");
+                }
+                _maxExecutionTime = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the name of the JSON-definition file.
+        /// </summary>
+        public FileInfo? FileName { get; set; }
+
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
