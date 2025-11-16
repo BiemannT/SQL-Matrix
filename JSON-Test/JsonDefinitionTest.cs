95d10612c8fc43ce93348d2154229ed717ec02a4
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-11-16 16:35:10 +0100

    First test method

---
+++ JSON-Test/JsonDefinitionTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace BiemannT.MUT.MsSql.Def.JSON.Test
+{
+    [TestClass]
+    public sealed class JsonDefinitionTest
+    {
+        /// <summary>
+        /// Test loading of an empty json-file.
+        /// </summary>
+        [TestMethod]
+        public void Test_LoadEmptyFile()
+        {
+            JsonDefinition jsonTest = new()
+            {
+                FileName = new("./Definitionen/Empty file.json")
+            };
+
+            // Bei der Methode Load wird eine Exception erwartet, mit einer innerException.
+            Exception result = Assert.ThrowsException<InvalidOperationException>(jsonTest.Load);
+
+            Assert.AreEqual("The test file definition contains invalid JSON code - or - required fields are missing!", result.Message);
+
+            Assert.IsInstanceOfType(result.InnerException, typeof(JsonException));
+
+        }
+    }
+}
