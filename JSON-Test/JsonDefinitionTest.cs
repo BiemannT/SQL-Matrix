7dd384ca9033e839069ba358433ddad3a5a6b4cd
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-11-16 20:56:06 +0100

    Add test invalid MaxExecutionTime

--- JSON-Test/JsonDefinitionTest.cs
+++ JSON-Test/JsonDefinitionTest.cs
@@ -67,5 +67,26 @@ namespace BiemannT.MUT.MsSql.Def.JSON.Test
 
             StringAssert.StartsWith(result.Message, "The API-Version 'v0' is not supported.");
         }
+
+        /// <summary>
+        /// Test loading of a definition file with a negative MaxExecutionTime.
+        /// In the test file all required properties are set.
+        /// </summary>
+        [TestMethod]
+        public void Test_LoadFileWithInvalidExecutionTime()
+        {
+            JsonDefinition jsonTest = new()
+            {
+                FileName = new("./Definitionen/Invalid execution time.json")
+            };
+
+            // Bei der Methode Load wird eine Exception erwartet mit der Nachricht, dass nur positive Zahlen erlaubt sind.
+            Exception result = Assert.ThrowsException<ArgumentOutOfRangeException>(jsonTest.Load);
+
+            TestContext.WriteLine("Actual load exception message: {0}", result.Message);
+
+            StringAssert.StartsWith(result.Message, "MaxExecutionTime must be a positive integer.");
+            Assert.AreEqual(-10, ((ArgumentOutOfRangeException)result).ActualValue);
+        }
     }
 }
