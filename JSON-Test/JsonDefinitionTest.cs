80b40cdce2f7ac386d46f1476ee9e28835163490
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-11-16 17:18:21 +0100

    Update JsonDefinitionTest.cs
    Add testcontext messages.

--- JSON-Test/JsonDefinitionTest.cs
+++ JSON-Test/JsonDefinitionTest.cs
@@ -5,6 +5,8 @@ namespace BiemannT.MUT.MsSql.Def.JSON.Test
     [TestClass]
     public sealed class JsonDefinitionTest
     {
+        public TestContext TestContext { get; set; }
+
         /// <summary>
         /// Test loading without a given file name.
         /// </summary>
@@ -16,6 +18,8 @@ namespace BiemannT.MUT.MsSql.Def.JSON.Test
             // Bei der Methode Load wird eine Exception erwartet.
 
             Exception result = Assert.ThrowsException<InvalidOperationException>(jsonTest.Load);
+
+            TestContext.WriteLine("Actual load exception message: {0}", result.Message);
 
             Assert.AreEqual("FileName is not set.", result.Message);
         }
@@ -34,9 +38,13 @@ namespace BiemannT.MUT.MsSql.Def.JSON.Test
             // Bei der Methode Load wird eine Exception erwartet, mit einer innerException.
             Exception result = Assert.ThrowsException<InvalidOperationException>(jsonTest.Load);
 
+            TestContext.WriteLine("Actual load exception message: {0}", result.Message);
+
             Assert.AreEqual("The test file definition contains invalid JSON code - or - required fields are missing!", result.Message);
 
             Assert.IsInstanceOfType(result.InnerException, typeof(JsonException));
+
+            TestContext.WriteLine("Inner Exception of type '{0}'. Message: {1}", result.InnerException.GetType().FullName, result.InnerException.Message);
 
         }
     }
