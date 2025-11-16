ff732910a9ded6d3614869035c18939ed6b399f9
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-11-16 18:35:35 +0100

    Add test Invalid Api version

--- JSON-Test/JsonDefinitionTest.cs
+++ JSON-Test/JsonDefinitionTest.cs
@@ -47,5 +47,25 @@ namespace BiemannT.MUT.MsSql.Def.JSON.Test
             TestContext.WriteLine("Inner Exception of type '{0}'. Message: {1}", result.InnerException.GetType().FullName, result.InnerException.Message);
 
         }
+
+        /// <summary>
+        /// Test loading of a definition file with an invalid API version number.
+        /// In the test file all required properties are set.
+        /// </summary>
+        [TestMethod]
+        public void Test_LoadFileWithInvalidApiVersion()
+        {
+            JsonDefinition jsonTest = new()
+            {
+                FileName = new("./Definitionen/Invalid API version.json")
+            };
+
+            // Bei der Methode Load wird eine Exception erwartet mit der Nachricht, dass es sich nicht um eine gültige Version handelt.
+            Exception result = Assert.ThrowsException<NotSupportedException>(jsonTest.Load);
+
+            TestContext.WriteLine("Actual load exception message: {0}", result.Message);
+
+            StringAssert.StartsWith(result.Message, "The API-Version 'v0' is not supported.");
+        }
     }
 }
