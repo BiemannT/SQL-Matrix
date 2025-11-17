1d004928847ff32215c64846ca393cab9ba2477b
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-11-17 21:20:12 +0100

    Create ExpectedResultTypes.cs

---
+++ Definition/Common/ExpectedResultTypes.cs
@@ -0,0 +1,23 @@
+﻿namespace BiemannT.MUT.MsSql.Def.Common
+{
+    /// <summary>
+    /// This enumeration represents possible types of expected test results.
+    /// </summary>
+    public enum ExpectedResultTypes
+    {
+        /// <summary>
+        /// Indicates that the test will return a single value.
+        /// </summary>
+        Value = 1,
+
+        /// <summary>
+        /// Indicates that the test will return one or more result sets.
+        /// </summary>
+        Resultset = 2,
+
+        /// <summary>
+        /// Indicates that the test will end with an expected exception.
+        /// </summary>
+        Exception = 3,
+    }
+}
