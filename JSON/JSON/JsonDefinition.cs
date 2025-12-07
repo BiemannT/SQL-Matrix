5d97ffba88741bf80ddc99e0d874140c215b31ab
Tobias Biemann <151792132+BiemannT@users.noreply.github.com>
2025-12-07 18:11:07 +0100

    Adopt JsonDefinition to virtual class

--- JSON/JSON/JsonDefinition.cs
+++ JSON/JSON/JsonDefinition.cs
@@ -7,116 +7,127 @@ namespace BiemannT.MUT.MsSql.Def.JSON
     /// <summary>
     /// This class provides properties and methods to interact with JSON-definition files.
     /// </summary>
-    public class JsonDefinition : Common.Definition
+    public class JsonDefinition : Definition
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="JsonDefinition"/> class with default values.
         /// </summary>
-        public JsonDefinition() {
-            // Initialize the class with default values.
-            ApiVersion = new(1, 0);
-            SchemaName = string.Empty;
-            ObjectName = string.Empty;
-            _maxExecutionTime = 10;
-            _inputs = [];
-            _expectedResults = [];
-        }
+        public JsonDefinition() : base() { }
 
         /// <summary>
-        /// Internal representation for the JSON Property of the API-Version.
+        /// <inheritdoc/>
         /// </summary>
-        /// <exception cref="NotSupportedException">Will be thrown in case of an invalid version number.</exception>
+        /// <remarks>
+        /// <inheritdoc/>
+        /// This property is required!
+        /// </remarks>
         [JsonPropertyName("SQL-Matrix-Api-Version")]
         [JsonPropertyOrder(-10)]
         [JsonRequired]
         [JsonInclude]
-        private string ApiVersionJson
+        [JsonConverter(typeof(JsonApiVersionConverter))]
+        public override Version ApiVersion
         {
-            get
-            {
-                return ApiVersion switch
-                {
-                    { Major: 1, Minor: 0 } => "v1",
-                    _ => string.Empty
-                };
-            }
-
-            set
-            {
-                ApiVersion = value switch
-                {
-                    "v1" => new(1, 0),
-                    _ => throw new NotSupportedException($"The API-Version '{value}' is not supported.")
-                };
-            }
+            get => base.ApiVersion;
+            protected set => base.ApiVersion = value;
         }
 
         /// <summary>
-        /// Gets the API version for the current instance.
+        /// <inheritdoc/>
         /// </summary>
-        /// <value>The API version. The default value is 1.0</value>
-        [JsonIgnore]
-        public override Version ApiVersion { get; protected set; }
-
-        /// <summary>
-        /// Gets or sets the schema name of the tested object.
-        /// </summary>
-        /// <value>The schema name of the tested object. The default value is <see cref="string.Empty"/>.</value>
-        /// <remarks>This property is required!</remarks>
+        /// <remarks>
+        /// <inheritdoc/>
+        /// This property is required!
+        /// </remarks>
         [JsonPropertyName("SchemaName")]
         [JsonPropertyOrder(-9)]
         [JsonRequired]
-        public override string SchemaName { get; set; }
+        public override string SchemaName
+        {
+            get => base.SchemaName;
+            set => base.SchemaName = value;
+        }
 
         /// <summary>
-        /// Gets or sets the name of the tested object.
+        /// <inheritdoc/>
         /// </summary>
-        /// <value>The name of the tested object. The default value is <see cref="string.Empty"/>.</value>
-        /// <remarks>This property is required!</remarks>
+        /// <remarks>
+        /// <inheritdoc/>
+        /// This property is required!
+        /// </remarks>
         [JsonPropertyName("TestObjectName")]
         [JsonPropertyOrder(-8)]
         [JsonRequired]
-        public override string ObjectName { get; set; }
-
-        private int _maxExecutionTime;
+        public override string ObjectName
+        {
+            get => base.ObjectName;
+            set => base.ObjectName = value;
+        }
 
         /// <summary>
-        /// Gets or sets the maximum execution time for each test case in seconds.
+        /// <inheritdoc/>
         /// </summary>
-        /// <value>
-        /// The maximum execution time in seconds.
-        /// The default value is 10 seconds. The value 0 means infinite execution time.
-        /// It is not recommended to set the value to 0 for test cases as the test case can freeze the overall testing.
-        /// </value>
-        /// <exception cref="ArgumentOutOfRangeException">Thrown when the value is less than zero.</exception>"
+        /// <remarks><inheritdoc/></remarks>
         [JsonPropertyName("MaxExecutionTime")]
         [JsonPropertyOrder(-7)]
         public override int MaxExecutionTime
         {
-            get => _maxExecutionTime;
-            set
-            {
-                if (value < 0)
-                {
-                    throw new ArgumentOutOfRangeException(nameof(MaxExecutionTime), value, "MaxExecutionTime must be a positive integer.");
-                }
-                _maxExecutionTime = value;
-            }
+            get => base.MaxExecutionTime;
+            set => base.MaxExecutionTime = value;
         }
 
         [JsonPropertyName("Inputs")]
         [JsonPropertyOrder(-6)]
         [JsonInclude]
-        private List<JsonInput> _inputs;
+        private List<JsonInput> JsonInputs
+        {
+            get
+            {
+                // Get Methode wird aufgerufen, wenn der JSON-Serializer die Inputs auslesen möchte.
+                // Dabei müssen die Basisklassen-Input-Objekte in JsonInput-Objekte umgewandelt werden.
+                List<JsonInput> inputs = [];
+                foreach (var input in base.Inputs)
+                {
+                    if (input is JsonInput jsonInput)
+                    {
+                        // Falls es bereits ein JsonInput-Objekt ist, einfach hinzufügen
+                        inputs.Add(jsonInput);
+                    }
+                    else
+                    {
+                        // Ansonsten ein neues JsonInput-Objekt erstellen und die Werte kopieren
+                        JsonInput newJsonInput = new()
+                        {
+                            ParameterName = input.ParameterName,
+                            SqlTypeDef = input.SqlTypeDef,
+                            IsNullable = input.IsNullable,
+                            HasDefaultValue = input.HasDefaultValue,
+                            Direction = input.Direction
+                        };
+                        newJsonInput.UserValues.AddRange(input.UserValues);
+                        inputs.Add(newJsonInput);
+                    }
+                }
+                return inputs;
+            }
+
+            set
+            {
+                // Set Methode wird aufgerufen, wenn der JSON-Deserializer die Inputs setzen möchte.
+                base.Inputs.Clear();
+                foreach (var input in value)
+                {
+                    base.Inputs.Add(input);
+                }
+            }
+        }
 
         /// <summary>
-        /// Gets the collection of input parameters.
+        /// <inheritdoc/>
         /// </summary>
+        /// <remarks><inheritdoc/></remarks>
         [JsonIgnore]
-        public override List<Common.Input> Inputs
-        {
-            get => [.. _inputs.Cast<Common.Input>()];
-        }
+        public override List<Input> Inputs { get => base.Inputs; }
 
         /// <summary>
         /// Gets or sets the name of the JSON-definition file.
@@ -160,8 +171,8 @@ namespace BiemannT.MUT.MsSql.Def.JSON
                 this.ApiVersion = _definition.ApiVersion;
                 this.SchemaName = _definition.SchemaName;
                 this.ObjectName = _definition.ObjectName;
-                this._maxExecutionTime = _definition._maxExecutionTime;
-                this._inputs = _definition._inputs;
+                this.MaxExecutionTime = _definition.MaxExecutionTime;
+                this.Inputs.AddRange(_definition.Inputs);
                 this._expectedResults = _definition._expectedResults;
             }
             catch (JsonException ex)
